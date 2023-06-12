c6af3310a9bec35520304f1fd52cd7887e812da4
Долгушин Антон <antodolg@gmail.com>
2023-06-12 15:03:05 +0800

    deploy18

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -9,7 +9,7 @@ from api.utils import download_cart
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
